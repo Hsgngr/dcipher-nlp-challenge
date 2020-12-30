f5942215e4717c9fa00508ebe869b988ae172f6b
Ege <hsgngr@gmail.com>
2020-12-30 04:24:52 +0000

    plots are added.

--- tf_data_preprocessing.py
+++ tf_data_preprocessing.py
@@ -108,7 +108,7 @@ validation_data_ds = validation_data_ds.cache().prefetch(buffer_size=AUTOTUNE)
 test_data_ds = test_data_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 #Create the model
-embedding_dim = 16
+embedding_dim = 50
 
 model = tf.keras.Sequential([
     layers.Embedding(max_features + 1, embedding_dim),
@@ -132,3 +132,40 @@ history = model.fit(
     validation_data = validation_data_ds,
     epochs = epochs)
 
+
+#Evaluate the model
+loss, accuracy = model.evaluate(test_data_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+###############################################################################
+#Create a plot of accuracy and loss over time
+history_dict = history.history
+history_dict.keys()
+
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
+plt.show()
